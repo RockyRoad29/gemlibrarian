b935705ffffdc3137a50cf6a7300e4f4578a9434
Michelle Baert <rocky.road@rocky-shore.net>
2016-10-19 14:09:28 +0200

    Promisification

--- index.js
+++ index.js
@@ -3,36 +3,47 @@
 var request = require('request');
 
 const GEMS_API =  "https://rubygems.org/api/v1/";
-var options = {
-  url: "https://rubygems.org/api/v1/gems/rails.json",
+const OPTIONS = {
   timeout: 1500,
   headers: {
   }
 };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    console.log(info);
-  } else {
-    console.log('error: '+ response.statusCode);
-    console.log(body);
-  }
+function p_request(options) {
+  return new Promise(function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // fulfills the promise with `body` as the value
+        return resolve(body);
+      } else {
+        // rejects the promise with `err` as the reason
+        return reject({error: error, response: response});
+      }
+    });
+  });
 }
-
 
 // GET - /api/v1/gems/[GEM NAME].(json|yaml)
 function show_gem(name) {
+  var options = Object.assign({}, OPTIONS);
   options.url = GEMS_API + `gems/${name}.json`;
-  request(options.url, callback);
+  return p_request(options).then(function(body){
+    var info = JSON.parse(body);
+    console.log(info);
+  });
 }
 
 // GET - /api/v1/search.(json|yaml)?query=[YOUR QUERY]
 function search(query) {
+  var options = Object.assign({}, OPTIONS);
   options.url = GEMS_API + "search.json?query=" + query;
-  request(options.url, callback);
-  // _.pick(info, some_keys);
+  return p_request(options).then(function(body){
+    var info = JSON.parse(body);
+    // _.pick(info, some_keys);
+    console.log(info);
+  });
 }
 
-// show_gem('devise');
-search('devise');
+show_gem('rails').then(function(){
+  search('devise');
+});
